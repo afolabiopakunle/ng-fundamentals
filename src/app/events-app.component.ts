c2fcb459cc2093f7a2fa748da86883bb19215780
Afolabi Opakunle <opakunleafolabi@gmail.com>
2021-05-08 05:57:59 +0100

    event card sample done

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -3,9 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'events-app',
   template: `
-  <h2>Hello World!</h2>
-  <img src="./assets/images/basic-shield.png" title='basic shield'>
-  `,
+  <events-list></events-list>`,
 })
 export class EventsAppComponent {
   title = 'ng-fundamentals';
